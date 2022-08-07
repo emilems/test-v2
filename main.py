b9480bb40d6942d19d30b1725f306e70b9591979
emilems <emile.massau@gmail.com>
2022-08-07 20:12:47 +0200

    Add statement

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("A")
+    print("B")
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
